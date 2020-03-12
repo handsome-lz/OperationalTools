ca39a25eb13cc0a4e0a2aba2197ccc1c40f13438
lzbr <3385008263@qq.com>
2020-03-12 22:07:08 +0800

    判断某命令是否在环境变量中

---
+++ inpath.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#author: liuzhe
+inpath () {
+if [ -n "$1" ] #参数校验，必须传入要查询的命令
+then
+  # 获取所有PATH
+	ALL_PATHS=$(echo "$PATH" | sed 's/:/ /g')
+	IFS=' '
+	ALL_PATHS=($ALL_PATHS) # 用默认的分隔符转换成数组
+	for ONE_PATH in "${ALL_PATHS[@]}"
+	do
+		if [ -f "$ONE_PATH/$1" ]
+		then
+			echo "${1} is existent"
+			return 0
+		else
+			continue
+		fi
+	done
+	echo "${1} not found"
+else
+echo "need params"
+fi
+
+}
+inpath $1
